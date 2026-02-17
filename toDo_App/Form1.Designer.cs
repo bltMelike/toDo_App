51e6b3627295c2259b6bac4ed6d7bd678537f60e
Melike <bulutmelike1876@gmail.com>
2026-02-17 15:49:14 +0300

    Add project files.

---
+++ toDo_App/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace toDo_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.kilavuzButon = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ekleButon = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bitirButon = new System.Windows.Forms.Button();
+            this.monthCalendar2 = new System.Windows.Forms.MonthCalendar();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.guncelleButon = new System.Windows.Forms.Button();
+            this.silButon = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabControl1.Location = new System.Drawing.Point(10, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(727, 521);
+            this.tabControl1.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.kilavuzButon);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.monthCalendar1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.ekleButon);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(719, 493);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Giriş";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // kilavuzButon
+            // 
+            this.kilavuzButon.Location = new System.Drawing.Point(198, 401);
+            this.kilavuzButon.Name = "kilavuzButon";
+            this.kilavuzButon.Size = new System.Drawing.Size(262, 63);
+            this.kilavuzButon.TabIndex = 9;
+            this.kilavuzButon.Text = "Kullanma Kılavuzu\r\n";
+            this.kilavuzButon.UseVisualStyleBackColor = true;
+            this.kilavuzButon.Click += new System.EventHandler(this.kilavuzButon_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(195, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tarih: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(195, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Görev: ";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(198, 126);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(198, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // ekleButon
+            // 
+            this.ekleButon.Location = new System.Drawing.Point(198, 345);
+            this.ekleButon.Name = "ekleButon";
+            this.ekleButon.Size = new System.Drawing.Size(262, 50);
+            this.ekleButon.TabIndex = 1;
+            this.ekleButon.Text = "Ekle";
+            this.ekleButon.UseVisualStyleBackColor = true;
+            this.ekleButon.Click += new System.EventHandler(this.ekleButon_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.bitirButon);
+            this.tabPage2.Controls.Add(this.monthCalendar2);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.guncelleButon);
+            this.tabPage2.Controls.Add(this.silButon);
+            this.tabPage2.Controls.Add(this.flowLayoutPanel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(719, 493);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Görüntüle";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(31, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Görevler: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(394, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Görev: ";
+            this.label1.Visible = false;
+            // 
+            // bitirButon
+            // 
+            this.bitirButon.Location = new System.Drawing.Point(397, 431);
+            this.bitirButon.Name = "bitirButon";
+            this.bitirButon.Size = new System.Drawing.Size(262, 36);
+            this.bitirButon.TabIndex = 12;
+            this.bitirButon.Text = "Bitir";
+            this.bitirButon.UseVisualStyleBackColor = true;
+            this.bitirButon.Visible = false;
+            this.bitirButon.Click += new System.EventHandler(this.bitirButon_Click);
+            // 
+            // monthCalendar2
+            // 
+            this.monthCalendar2.Location = new System.Drawing.Point(397, 200);
+            this.monthCalendar2.Name = "monthCalendar2";
+            this.monthCalendar2.TabIndex = 11;
+            this.monthCalendar2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(444, 150);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(215, 22);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Visible = false;
+            // 
+            // guncelleButon
+            // 
+            this.guncelleButon.Location = new System.Drawing.Point(394, 85);
+            this.guncelleButon.Name = "guncelleButon";
+            this.guncelleButon.Size = new System.Drawing.Size(268, 36);
+            this.guncelleButon.TabIndex = 8;
+            this.guncelleButon.Text = "Güncelle";
+            this.guncelleButon.UseVisualStyleBackColor = true;
+            this.guncelleButon.Click += new System.EventHandler(this.guncelleButon_Click);
+            // 
+            // silButon
+            // 
+            this.silButon.Location = new System.Drawing.Point(394, 44);
+            this.silButon.Name = "silButon";
+            this.silButon.Size = new System.Drawing.Size(268, 35);
+            this.silButon.TabIndex = 7;
+            this.silButon.Text = "Sil";
+            this.silButon.UseVisualStyleBackColor = true;
+            this.silButon.Click += new System.EventHandler(this.silButon_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(34, 47);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(351, 432);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(71, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(605, 488);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Talimatlar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(89, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(428, 170);
+            this.label6.TabIndex = 11;
+            this.label6.Text = resources.GetString("label6.Text");
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(156, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(293, 44);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Kapat";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 544);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Yapılacaklar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button kilavuzButon;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ekleButon;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bitirButon;
+        private System.Windows.Forms.MonthCalendar monthCalendar2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button guncelleButon;
+        private System.Windows.Forms.Button silButon;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ toDo_App/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace toDo_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ArrayList gorevKutulari = new ArrayList();
+        ArrayList tarihler = new ArrayList();
+        int sayac = 0;
+        String text;
+        DateTime tarih;
+        int tarihSimdi = DateTime.Now.DayOfYear;
+        int kalanSure;
+        int seciliIndex;
+        public void satirEkle(String text, String tarih,int kalanSure)
+        {
+            sayac++;
+            CheckBox checkBox = new CheckBox();
+
+            checkBox.Name = "ChechkBox" + sayac;
+            checkBox.Text = text + " / " + tarih + " / Bitişe kalan: " + kalanSure + " gün";
+            checkBox.Checked = false;
+            checkBox.AutoSize = true;
+            gorevKutulari.Add(checkBox);
+            tarihler.Add(tarih);
+        }
+        public void goruntule()
+        {
+            flowLayoutPanel1.Controls.Clear();
+            for (int i = 0; i < gorevKutulari.Count; i++)
+            {
+
+                flowLayoutPanel1.Controls.Add((CheckBox)gorevKutulari[i]);
+            }
+        }
+
+        private void ekleButon_Click(object sender, EventArgs e)
+        {
+            text = textBox1.Text;
+            tarih = monthCalendar1.SelectionStart.Date;
+            String tarih2= monthCalendar1.SelectionStart.ToString("d");
+            
+            kalanSure = tarih.DayOfYear - tarihSimdi;
+
+            if (text != "" && kalanSure >= 0)
+            {
+                satirEkle(text, tarih2,kalanSure);
+                goruntule();
+                textBox1.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("!!!Lütfen girilen bilgileri kontrol ediniz!!!", "Mesaj", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+        }
+
+        private void kilavuzButon_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+            tabControl1.Visible = false;
+        }
+
+        private void silButon_Click(object sender, EventArgs e)
+        {
+            for (int i = gorevKutulari.Count - 1; i >= 0; i--)
+            {
+                CheckBox checkBox_Yedek = new CheckBox();
+                checkBox_Yedek = (CheckBox)gorevKutulari[i];
+                if (checkBox_Yedek.Checked == true)
+                {
+                    gorevKutulari.RemoveAt(i);
+                }
+            }
+            goruntule();
+        }
+
+        private void guncelleButon_Click(object sender, EventArgs e)
+        {
+            int sayacKontrol = 0;
+            CheckBox checkBox_Yedek = new CheckBox();
+
+
+            for (int i = gorevKutulari.Count - 1; i >= 0; i--)
+            {
+                checkBox_Yedek = (CheckBox)gorevKutulari[i];
+                if (checkBox_Yedek.Checked == true)
+                {
+                    sayacKontrol++;
+
+                }
+                if (sayacKontrol == 1)
+                {
+                    seciliIndex = i;
+                }
+                else
+                {
+                    seciliIndex = -1;
+                }
+            }
+            if (sayacKontrol > 1)
+            {
+                MessageBox.Show("!!!Sadece 1 tane seçebilirsiniz!!!");
+            }
+            if (sayacKontrol == 0)
+            {
+                MessageBox.Show("!!!Seçim yapın!!!");
+            }
+            else
+            {
+                textBox3.Visible = true;
+                label1.Visible = true;
+                bitirButon.Visible = true;
+                flowLayoutPanel1.Enabled = false;
+                silButon.Enabled = false;
+                monthCalendar2.Visible = true;
+            }
+            textBox3.Text = "";
+        }
+
+        private void bitirButon_Click(object sender, EventArgs e)
+        {
+            CheckBox checkBox_Yedek = new CheckBox();
+            checkBox_Yedek = (CheckBox)gorevKutulari[seciliIndex];
+
+            text = textBox3.Text;
+            tarih = monthCalendar2.SelectionStart.Date;
+            String tarih2 = monthCalendar2.SelectionStart.ToString("d");
+            kalanSure = tarih.DayOfYear - tarihSimdi;
+            
+            
+            if (text != "" && kalanSure >= 0)
+            {
+                checkBox_Yedek.Text = text + " / " + tarih2 + " / Bitişe kalan: " + kalanSure + " gün";
+                goruntule();
+                checkBox_Yedek.Checked = false;
+                textBox3.Visible = false;
+                label1.Visible = false;
+                monthCalendar2.Visible = false;
+                bitirButon.Visible = false;
+                flowLayoutPanel1.Enabled = true;
+                silButon.Enabled = true;
+                guncelleButon.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("!!!Lütfen sadece birini değiştirecek olsanız bile iki parametreyi de giriniz!!!", "Mesaj", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("!!!Girilen bilgileri kontrol ediniz!!!", "Mesaj", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DialogResult cevap = MessageBox.Show("Hoşgeldiniz...\nUygulamamızı ilk defa mı kullanıyorsunuz?", "Mesaj", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (cevap == DialogResult.Yes)
+            {
+                groupBox1.Visible = true;
+                tabControl1.Visible = false;
+
+            }
+            else if (cevap == DialogResult.No)
+            {
+                groupBox1.Visible = false;
+                tabControl1.Visible = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            tabControl1.Visible = true;
+        }
+    }
+}
